2e7e9e3a7e092ae138c259006e20183de722c0a7
Eric Sod <esod@northps.com>
2013-12-17 15:16:33 -0500

    adds featured_work module

---
+++ sites/all/modules/custom/featured_work/theme/featured-work-block.tpl.php
@@ -0,0 +1,55 @@
+<h3>Featured Work</h3>
+  <ul id="projects-carousel" class="loading">
+
+  <?php
+    $result = array();
+    $output = '';
+      
+    foreach ($keys as $id => $key) {
+      $result[$key] = array(
+        'portfolio_image' => $portfolio_image[$id],
+        'alt_title' => $alt_title[$id],
+        'thumbnail_image' => $thumbnail_image[$id],
+        'which_node' => $which_node[$id],
+        'caption_title' => $caption_title[$id],
+        'project_type' => $project_type[$id],
+      );
+      //var_dump($result[$key]);die;
+    }
+    
+    foreach ($result as $key => $value) {
+      $portfolio_image = $value['portfolio_image'];
+      $alt_title = $value['alt_title'];
+      $thumbnail_image = $value['thumbnail_image'];
+      $which_node = $value['which_node'];
+      $caption_title = $value['caption_title'];
+      $project_type = $value['project_type'];
+  
+      $output = '<!-- PROJECT ITEM STARTS -->';
+      $output .= '<li>';
+      $output .= '<div class="item-content">';
+      $output .= '<div class="link-holder">';
+      $output .= '<div class="portfolio-item-holder">';
+      $output .= '<div class="portfolio-item-hover-content">';
+      $output .= '<a href="' . base_path() . drupal_get_path('theme', 'simplecorp') . '/images/sampleimages/'.$portfolio_image.'" alt="'.$alt_title.'" title="'.$alt_title.'" data-rel="prettyPhoto" class="zoom">';
+      $output .= '<img src="" alt="'.$alt_title.'" /></a>';
+      $output .= '<img src="' . base_path() . drupal_get_path('theme', 'simplecorp') . '/images/sampleimages/'.$thumbnail_image.'" alt="'.$alt_title.'" width="220"  class="portfolio-img" />';
+      $output .= '<div class="hover-options"></div>';
+      $output .= '</div>';
+      $output .= '</div>';
+      $output .= '<div class="description">';
+      $output .= '<p><a href="' . base_path() . $which_node . '" title="title"> ' . $caption_title . '</a></p>';
+      $output .= '<span>'.$project_type.'</span>';
+      $output .= '</div>';
+      $output .= '</div>';
+      $output .= '</div>';
+      $output .= '</li>';
+      $output .= '<!-- PROJECT ITEM ENDS -->';
+      
+      print $output;
+    }
+  ?>
+
+  </ul>
+    <!-- // optional "view full portfolio" button on homepage featured projects -->
+    <a href="<?php print base_path() . 'full-portfolio'; ?>" class="colored" title="portofolio">View full portofolio</a>
