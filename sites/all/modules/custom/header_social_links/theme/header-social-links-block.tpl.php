6aa00fe5be12be024cc4e40c2b1c94a46e2e302a
Eric Sod <esod@northps.com>
2013-12-19 13:27:27 -0500

    better design pattern for custom modules

---
+++ sites/all/modules/custom/header_social_links/theme/header-social-links-block.tpl.php
@@ -0,0 +1,15 @@
+<!-- #socialIcons -->
+<div id="social-icons" class="clearfix">
+  <ul id="social-links">
+    <li class="facebook-link"><a href="https://www.facebook.com/eric.sod" class="facebook" id="social-01" title="Join Me on Facebook!">Facebook</a></li>
+    <li class="twitter-link"><a href="https://twitter.com/EricSod" class="twitter" id="social-02" title="Follow Me on Twitter">Twitter</a></li>
+    <li class="google-link"><a href="#" id="social-03" title="Google" class="google">Google</a></li>
+    <li class="dribbble-link"><a href="http://dribbble.com/EricSod" id="social-04" title="Dribble" class="dribbble">Dribble</a></li>
+    <li class="vimeo-link"><a href="https://vimeo.com/user10386020" id="social-05" title="Vimeo" class="vimeo">Vimeo</a></li>
+    <li class="skype-link"><a href="http://www.skype.com/en/" id="social-06" title="Skype" class="skype">Skype</a></li>
+    <li class="linkedin-link"><a href="http://www.linkedin.com/in/ericsod" id="social-07" title="Linkedin" class="linkedin">Linkedin</a></li>
+    <li class="pinterest-link"><a href="#" id="social-09" title="Pinterest" class="pinterest">Pinterest</a></li>
+    <li class="rss-link"><a href="#" id="social-08" title="RSS" class="rss">RSS Feeds</a></li>
+  </ul>
+</div>
+<!-- EOF: #socialIcons -->
