a6070508321dae4d14c50b5604f06a3e36404f32
Eric Sod <esod@northps.com>
2013-12-19 14:06:23 -0500

    add inner_page_images module

---
+++ sites/all/modules/custom/inner_page_images/theme/inner-page-image-blog-entries-block.tpl.php
@@ -0,0 +1,3 @@
+<div class="container clearfix ">
+  <img class="intro-img" alt=" " src="<?php print base_path() . drupal_get_path('theme', 'simplecorp') ;?>/images/sampleimages/technology_landing.jpg">
+</div>
