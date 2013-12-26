9e071d57f87a879790f8b5833755a0e003494df9
Eric Sod <esod@northps.com>
2013-12-26 11:34:59 -0500

    add custom_slideshow.js and so forth

---
+++ sites/all/themes/simplecorp/js/custom_slideshow.js
@@ -0,0 +1,36 @@
+(function ($) {
+  Drupal.behaviors.custom_slideshow_data = {
+    attach: function() {
+  
+      var effect = Drupal.settings.custom_slideshow_array.effect;
+      var effect_time = Drupal.settings.custom_slideshow_array.effect_time;
+      var slideshow_controls = Drupal.settings.custom_slideshow_array.slideshow_controls;
+      var slideshow_random = Drupal.settings.custom_slideshow_array.slideshow_random;
+      var slideshow_pause = Drupal.settings.custom_slideshow_array.slideshow_pause;
+      var slideshow_touch = Drupal.settings.custom_slideshow_array.slideshow_touch;
+    
+      $(window).load(function() {
+
+        $(".flexslider").fadeIn("slow");
+
+        $(".flexslider").flexslider({
+          useCSS: false,
+          animation: effect,
+          controlNav: slideshow_controls,
+          directionNav: slideshow_controls,
+          animationLoop: true,
+          touch: slideshow_touch,
+          pauseOnHover: slideshow_pause,
+          nextText: "&rsaquo;",
+          prevText: "&lsaquo;",
+          keyboard: true,
+          slideshowSpeed: effect_time,
+          randomize: slideshow_random,
+          start: function(slider) {
+            slider.removeClass("loading");
+          }
+        });
+      });
+    }
+  };
+}(jQuery));
