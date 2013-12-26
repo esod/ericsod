8929d526f3431e47452f64db19e6c71f6c1baea7
Eric Sod <esod@northps.com>
2013-12-26 13:23:22 -0500

    add custom_contact_map module

---
+++ sites/all/modules/custom/custom_contact_map/theme/custom_contact_map.tpl.php
@@ -0,0 +1,3 @@
+<div id="contact-map">
+<iframe height="350" src="https://maps.google.com/maps?f=q&amp;source=s_q&amp;hl=en&amp;geocode=&amp;q=230+Ashland+Place,+Brooklyn,+NY,+United+States&amp;aq=0&amp;oq=230+Ashland+Place&amp;sll=40.697488,-73.979681&amp;sspn=1.086989,1.454315&amp;ie=UTF8&amp;hq=&amp;hnear=230+Ashland+Place,+Brooklyn,+11217&amp;t=m&amp;z=14&amp;ll=40.688318,-73.978586&amp;output=embed&amp;iwloc=&amp;"></iframe>
+</div>
