7dd64e23fda3f4f0c98ed526426461c2e3395010
Eric Sod <esod@northps.com>
2013-12-26 11:13:40 -0500

    change jQuery  to $ in featured_work.js since Drupal namespaces are being used

--- sites/all/themes/simplecorp/js/featured_work.js
+++ sites/all/themes/simplecorp/js/featured_work.js
@@ -5,9 +5,9 @@
       var carousel_effect_time = Drupal.settings.featured_work_array.carousel_effect_time;
       var carousel_effect = Drupal.settings.featured_work_array.carousel_effect;
   
-      var currentWindowWidth = jQuery(window).width();
-      jQuery(window).resize(function() {
-        currentWindowWidth = jQuery(window).width();
+      var currentWindowWidth = $(window).width();
+      $(window).resize(function() {
+        currentWindowWidth = $(window).width();
       });
       
       $(window).load(function() {
@@ -15,24 +15,24 @@
         $("ul#projects-carousel").fadeIn("fast");
         
         // Sets the behavior for the ball drops
-        if (jQuery(".portfolio-item-hover-content").length && jQuery()) {
+        if ($(".portfolio-item-hover-content").length && $()) {
           function hover_effect() {
-            jQuery(".portfolio-item-hover-content").hover(function() {
-              jQuery(this).find("div,a").stop(0, 0).removeAttr("style");
-              jQuery(this).find(".hover-options").animate({
+            $(".portfolio-item-hover-content").hover(function() {
+              $(this).find("div,a").stop(0, 0).removeAttr("style");
+              $(this).find(".hover-options").animate({
                 opacity: 0.9
               }, "fast");
-              jQuery(this).find("a").animate({
+              $(this).find("a").animate({
                 "top": "60%"
               });
             }, function() {
-              jQuery(this).find(".hover-options").stop(0, 0).animate({
+              $(this).find(".hover-options").stop(0, 0).animate({
                 opacity: 0
               }, "fast");
-              jQuery(this).find("a").stop(0, 0).animate({
+              $(this).find("a").stop(0, 0).animate({
                 "top": "150%"
               }, "slow");
-              jQuery(this).find("a.zoom").stop(0, 0).animate({
+              $(this).find("a.zoom").stop(0, 0).animate({
                 "top": "150%"
               }, "slow");
             });
@@ -42,14 +42,14 @@
         
         // Sets responsive behavior for the scroll
         (function() {
-          var jQuerycarousel = jQuery("#projects-carousel");
+          var jQuerycarousel = $("#projects-carousel");
           if (jQuerycarousel.length) {
             var scrollCount;
-            if (jQuery(window).width() < 480) {
+            if ($(window).width() < 480) {
               scrollCount = 1;
-            } else if (jQuery(window).width() < 768) {
+            } else if ($(window).width() < 768) {
               scrollCount = 1;
-            } else if (jQuery(window).width() < 960) {
+            } else if ($(window).width() < 960) {
               scrollCount = 3;
             } else {
               scrollCount = 4;
