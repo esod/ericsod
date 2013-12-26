e31164933fd30457213b07d9c176d82f06e7ec52
Eric Sod <esod@northps.com>
2013-12-26 11:50:09 -0500

    move js from theme to modules

---
+++ sites/all/modules/custom/header_social_links/js/header_social_links.js
@@ -0,0 +1,23 @@
+(function ($) {
+  Drupal.behaviors.header_social_links_data = {
+    attach: function() {
+  
+      if (jQuery().tipsy) {
+        jQuery("#social-01").tipsy({ gravity: "n" });
+        jQuery("#social-02").tipsy({ gravity: "n" });
+        jQuery("#social-03").tipsy({ gravity: "n" });
+        jQuery("#social-04").tipsy({ gravity: "n" });
+        jQuery("#social-05").tipsy({ gravity: "n" });
+        jQuery("#social-06").tipsy({ gravity: "n" });
+        jQuery("#social-07").tipsy({ gravity: "n" });
+        jQuery("#social-07").tipsy({ gravity: "n" });
+        jQuery("#social-08").tipsy({ gravity: "n" });
+        jQuery("#social-09").tipsy({ gravity: "n" });
+        jQuery("#social-10").tipsy({ gravity: "n" });
+        jQuery("#social-11").tipsy({ gravity: "n" });
+        jQuery("#team-01").tipsy({ gravity: "s" });
+      }
+     
+    }
+  };
+}(jQuery));
