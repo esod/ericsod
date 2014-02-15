ebfebe14682cf745c9b265868e856c0963ff5d68
Eric Sod <esod@northps.com>
2014-02-15 18:03:26 -0500

    Open the blog for comments. eek!

--- sites/all/modules/custom/custom_footer/theme/footer_fourth_block.tpl.php
+++ sites/all/modules/custom/custom_footer/theme/footer_fourth_block.tpl.php
@@ -1,9 +1,10 @@
 <div class="contactmap contact-info">
 <p></p>
 <address>
-<span class="address">230 Ashland Place, Brooklyn, NY</span>
+<span class="address">Brooklyn, NY</span>
 <span class="phone">(718) 864-8600</span>
-<span class="email">esod124@gmail.com</span>
+<span class="email"><a href="mailto:esod124@gmail.com">esod124@gmail.com</a></span>
+
 </address>
 <p></p>
 </div>
