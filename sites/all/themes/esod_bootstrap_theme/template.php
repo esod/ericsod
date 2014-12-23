e32ce3933f7b09db28585e423f1732fa66aec3ca
Eric Sod <esod@northpointdigital.com>
2014-12-23 16:56:27 -0500

    Removes unnecessary stuff from printing

--- sites/all/themes/esod_bootstrap_theme/template.php
+++ sites/all/themes/esod_bootstrap_theme/template.php
@@ -4,3 +4,26 @@
  * @file
  * template.php
  */
+/**
+ * Preprocess page functions
+ *
+ * @param $vars
+ */
+function esod_bootstrap_theme_preprocess_page(&$vars) {
+  if (drupal_is_front_page()) {
+    unset($vars['page']['content']['system_main']['default_message']); //will remove message "no front page content is created"
+    drupal_set_title(''); //removes welcome message (page title)
+  }
+}
+
+/**
+ * Preprocess node functions
+ *
+ * @param $vars
+ */
+function esod_bootstrap_theme_preprocess_node(&$vars) {
+  if (variable_get('node_submitted_' . $vars['node']->type, TRUE)) {
+    $date = format_date($vars['node']->created, 'date_type');
+    $vars['submitted'] = t('Submitted on !datetime', array('!datetime' => $date));
+  }
+}
